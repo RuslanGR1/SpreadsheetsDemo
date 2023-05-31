113ebf8966689e6b93f2da0a0d9c95ac5d9834fe
Gazizov Ruslan <almetfly@gmail.com>
2023-05-31 04:26:41 +0300

    feat: add spreadsheet component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SpreadsheetComponent } from './components/spreadsheet/spreadsheet.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SpreadsheetComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/spreadsheet/spreadsheet.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spreadsheet',
+  templateUrl: './spreadsheet.component.html',
+  styleUrls: ['./spreadsheet.component.css']
+})
+export class SpreadsheetComponent {}
